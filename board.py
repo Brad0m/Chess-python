f5956f2e09448ba4b2eb89c1c218a8218b5cad44
Brayan Dominguez <b942smart@gmail.com>
2020-04-16 19:18:39 -0500

    Add letters and numbers to the board when printing and print board in the center

--- board.py
+++ board.py
@@ -1,23 +1,39 @@
-def create_board():
+class Board:
 
-    board = [[]] * 8
-    pawn = ['W♙'] * 8
-    for element in range(8):
-        if element == 0: board[element] = ['B♖','B♘','B♗','B♕','B♔','B♗','B♘','B♖']
-        elif element == 7 : board[element] = ['W♖','W♘','W♗','W♕','W♔','W♗','W♘','W♖']
-        elif element == 1: board[element] = pawn
-        elif element == 6 : board[element] = pawn
-        else: board[element] = ["  "] * 8
-
-    return board
+    def __init__(self):
+        self.letters = ["A", "B", "C", "D", "E", "F", "G", "H"]
 
 
-def show_board(board):
+    def create_board(self):
 
-    for row in board:
-        for column in row:
-            print("   |   ", column, end="  ")
-        print("   | \n")
+        board = [[]] * 8
+        pawn = ['W♙'] * 8
+        for element in range(8):
+            if element == 0: board[element] = ['B♖','B♘','B♗','B♕','B♔','B♗','B♘','B♖']
+            elif element == 7 : board[element] = ['W♖','W♘','W♗','W♕','W♔','W♗','W♘','W♖']
+            elif element == 1: board[element] = pawn
+            elif element == 6 : board[element] = pawn
+            else: board[element] = ["  "] * 8
+
+        return board
 
 
-show_board(create_board())
+    def show_board(self, board):
+
+        num_row = 8
+
+        for row in board:
+            print(" " * 30 ,end= "{}".format(num_row))
+            for column in row:
+                print("   |   ", column, end="  ")
+
+            print("   | \n")
+            num_row -= 1
+            
+        print(end=" " * 29)
+        for letter in self.letters:
+            print(" " * 10, letter, end="")
+
+
+tablero = Board()
+tablero.show_board(tablero.create_board())
